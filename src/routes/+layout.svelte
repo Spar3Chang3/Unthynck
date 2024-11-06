cc115677f3aca714ec26b223cbe92aae596b5bce
spar3chang3 <baker.daryk@ic.edu>
2024-11-05 18:38:30 -0600

    Initial commit with global variables

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<style>
+    :root {
+        --background-standard: url("../../static/standard-background.jpg");
+        --surface-standard: #1f1f1f;
+        --primary-color: #0d0500;
+        --on-primary-color: #F8F0e3;
+        --secondary-color: #784895;
+        --on-secondary-color: #e4ebf7;
+
+        --primary-container: #140800;
+        --secondary-container: #7a2abf;
+
+        --paragraph-font-size: 1.4rem;
+        --paragraph-line-height: 1.5;
+
+        @media only screen and (max-width: 768px) {
+            --paragraph-font-size: 1.15rem;
+        }
+    }
+</style>
